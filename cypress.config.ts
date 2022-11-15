f958a2feab61b01130617e51ab4f28a815ecadd6
Mehradi <mehradi.info@gmail.com>
2022-11-15 17:03:56 +0200

    verifing correct request and response

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   env: {
-    baseUrl: 'http://localhost:4200/',
+    baseUrl: 'http://localhost:4200',
     username: 'cytest@test.com', 
     password: 'Welcome123',
     apiUrl: 'https://api.realworld.io'
--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -8,4 +8,23 @@ describe('Tests with backend', () => {
     it('should log in', async ()=>{
         cy.log('we logged in');
     });
+
+    it('verify correct request and response', () => {
+
+        cy.intercept('POST', 'https://api.realworld.io/api/articles/').as('postArticles')
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('This is the title')
+        cy.get('[formcontrolname="description"]').type('This is a description')
+        cy.get('[formcontrolname="body"]').type('This is a body of the article')
+        cy.contains('Publish Article').click()
+
+        cy.wait('@postArticles').then( xhr => {
+            console.log(xhr)
+            expect(xhr.response.statusCode).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is a body of the article')
+            expect(xhr.response.body.article.description).to.equal('This is a description')
+        })
+        
+    })
 })
